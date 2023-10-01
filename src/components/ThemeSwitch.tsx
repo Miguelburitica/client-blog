f0e04d93d82be62d6e3191a24e399bd559cda97d
Miguelburitica <miguelaburiticav@gmail.com>
2023-09-30 22:51:58 -0500

    fix: a little detail

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -34,7 +34,7 @@ const ThemeSwitcher = () => {
   return (
     <div class="relative inline-block text-left">
       <div>
-        <button onClick={() => setIsOpen(!isOpen)} type="button" id="menu-button" aria-expanded="true" aria-haspopup="true" class="flex justify-center items-center gap-2 w-full rounded-md border-none px-4 py-2 dark:bg-secondary-100 dark:text-white text-sm font-medium dark:hover:bg-gray-200 transition-colors focus:outline-none focus:bg-secondary-900 dark:focus:bg-secondary-200 bg-white hover:bg-secondary-900 text-secondary-300">
+        <button onClick={() => setIsOpen(!isOpen)} type="button" id="menu-button" aria-expanded="true" aria-haspopup="true" class="flex justify-center items-center gap-2 w-full rounded-md border-none px-4 py-2 dark:bg-secondary-100 dark:text-white text-sm font-medium dark:hover:bg-secondary-200 transition-colors focus:outline-none focus:bg-secondary-900 dark:focus:bg-secondary-200 bg-white hover:bg-secondary-900 text-secondary-300">
           Theme
           <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
