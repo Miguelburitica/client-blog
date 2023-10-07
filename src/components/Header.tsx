2f7ec0431813fd66cff05d010c96e624161483e4
Miguelburitica <miguelaburiticav@gmail.com>
2023-10-06 21:24:46 -0500

    advance in mobile experience

---
+++ src/components/Header.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'preact/hooks'
+import ThemeSwitcher from './ThemeSwitch'
+
+export default function Header() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+	function toggleMenu() {
+		setIsMenuOpen(!isMenuOpen)
+	}
+	
+	return (
+		<nav class="flex justify-center w-full fixed backdrop-blur-sm top-0 shadow-md dark:shadow-secondary-200 px-2">
+			<div class="flex justify-between items-center max-w-6xl w-full py-4">
+				<h2 class="text-4xl">Miguel</h2>
+				<ul class=" hidden md:flex gap-4">
+					<li class="hover:text-primary transition-colors flex items-center justify-center">
+						<a href="/#about">
+							Home
+						</a>
+					</li>
+					<li class="hover:text-primary transition-colors flex items-center justify-center">
+						<a href="/#projects">
+							Projects
+						</a>
+					</li>
+					<li class="hover:text-primary transition-colors flex items-center justify-center">
+						<a href="/interestes">
+							Personal Interests
+						</a>
+					</li>
+					<ThemeSwitcher />
+				</ul>
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-14 aspect-square md:hidden hover:bg-secondary-900 dark:hover:bg-secondary-200 transition-all rounded-full p-2 cursor-pointer" onClick={toggleMenu} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+				</svg>
+				<div className={`flex flex-col md:hidden w-full h-screen bg-primary-900 dark:bg-secondary-200 fixed top-0 left-0 z-50 transition-all duration-300 ease-linear ${isMenuOpen ? 'translate-x-0 left-0' : 'translate-x-full'}`}>
+					<div className="flex justify-between items-center py-4 shadow-md dark:shadow-secondary-100 px-2">
+						<h2 className="text-4xl">Miguel</h2>
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-14 aspect-square transition-all rounded-full p-2 cursor-pointer hover:bg-secondary-900 dark:hover:bg-secondary-100" onClick={toggleMenu} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</div>
+					<ul className="flex flex-col pt-8 items-center justify-center">
+						<li class="w-full">
+							<a href="/#about" class="text-secondary-300 hover:bg-primary dark:text-white dark:hover:bg-secondary transition-all px-4 py-3 flex justify-start">
+								Home
+							</a>
+						</li>
+						<li class="w-full">
+							<a href="/#projects" class="text-secondary-300 hover:bg-primary dark:text-white dark:hover:bg-secondary transition-all px-4 py-3 flex justify-start">
+								Projects
+							</a>
+						</li>
+						<li class="w-full mb-4">
+							<a href="/interestes" class="text-secondary-300 hover:bg-primary dark:text-white dark:hover:bg-secondary transition-all px-4 py-3 flex justify-start">
+								Personal Interests
+							</a>
+						</li>
+						<ThemeSwitcher />
+					</ul>
+				</div>
+			</div>
+		</nav>
+	)
+}
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -33,22 +33,18 @@ export default function ProjectCard({image, coverImage, name, githubUrl, demoUrl
   }
   
   return (
-    // <div class="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
-    // <article class={`flex aspect-video rounded-lg overflow-hidden shadow-md relative border-2 ${stack === 'front' ? ' border-emerald-400' : ' border-sky-400'}`} id="project-card" onMouseOver={hoverIn} onMouseLeave={hoverOut}>
-    <article class={`flex aspect-video rounded-lg overflow-hidden relative `} id="project-card" onMouseOver={hoverIn} onMouseLeave={hoverOut}>
-      <div class="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-      <div className="flex gap-4 flex-wrap items-center justify-center">
-        ({technologies.map((technology, index) => (
-          <span style={`transition-delay: ${index * 70}ms`} class={`transition-all duration-500 ease-in-out z-10 ${isHovered ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'}`}>
-            <div class={`[word-wrap: break-word] my-[5px] flex h-[32px] cursor-pointer items-center justify-between rounded-[16px] text-white border-2 border-${technology.color}-800 backdrop-filter backdrop-blur-sm bg-opacity-30 bg-primary-700 px-[12px] py-0 text-[13px] font-normal normal-case leading-loose shadow-none transition-[opacity] duration-300 ease-linear hover:border-${technology.color}-700 hover:!shadow-none`}>
+    <article class={`flex aspect-video rounded-lg overflow-hidden shadow-sm relative border-2 ${stack === 'front' ? ' border-emerald-400' : ' border-sky-400'}`} id="project-card" onMouseOver={hoverIn} onMouseLeave={hoverOut}>
+      <div className="flex gap-4 flex-wrap items-center justify-start w-full h-fit mt-auto mb-2 pl-2">
+        {technologies.map((technology, index) => (
+          <span style={`transition-delay: ${index * 70}ms`} class={`transition-all duration-500 ease-in-out z-10 h-fit ${isHovered ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'}`}>
+            <div class={`[word-wrap: break-word] flex h-[32px] items-center justify-between rounded-[16px] text-white border-2 border-${technology.color}-800 backdrop-filter backdrop-blur-sm bg-opacity-30 bg-primary-700 px-[12px] py-0 text-[13px] font-normal normal-case leading-loose shadow-none transition-[opacity] duration-300 ease-linear hover:border-${technology.color}-700 hover:!shadow-none`}>
               { technology.name }
             </div>
           </span>
-        ))})
+        ))}
       </div>
       <img class="absolute top-0 w-full" src={image} alt={`An preview of the project named ${name}`}/>
       <img class={`absolute top-0 w-full transition-transform ease-in ${isHovered ? 'translate-y-0' : '-translate-y-full'}`} src={coverImage} alt={`An cover preview of the project named ${name}`}/>
-      {/* Github button */}
       <button onClick={redirectGithub} class="absolute right-2 bottom-2 bg-[#171515] text-white px-4 py-2 rounded border border-gray-700 hover:bg-black focus:outline-none focus:ring-2 focus:ring-gray-600 active:bg-gray-800 flex items-center gap-2">
         <img class="w-6 aspect-square" src="github-mark-white.svg" alt="" />
         <img class="h-6" src="GitHub_Logo_White.png" alt="" srcset="" />
